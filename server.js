a863b15328136a2f9d8e18dff8b91a6cd2a832cc
Siddharth-IT59 <65977324+Siddharth-IT59@users.noreply.github.com>
2020-08-30 17:08:38 +0530

    cart,login,register

--- server.js
+++ server.js
@@ -4,19 +4,28 @@ const app = express()
 
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
-
 const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '/public')))
+app.use(expressLayout)
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname,'/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
 
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
