0c9f64df0c52fae339ef2cee25eb1c5a5eb308da
Siddharth-IT59 <65977324+Siddharth-IT59@users.noreply.github.com>
2020-10-22 00:37:15 +0530

    fixed admin markup 🛠

--- routes/web.js
+++ routes/web.js
@@ -8,53 +8,53 @@ const statusController = require('../app/http/controllers/admin/statusController
 const guest = require('../app/http/middleware/guest')
 const auth = require('../app/http/middleware/auth')
 const admin = require('../app/http/middleware/admin')
-const sharp = require('sharp')
 
 const multer = require('multer')
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/img/menu/')
-    },
-    filename: async function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-const upload = multer({ storage: storage})
+  destination: function (req, file, cb) {
+    cb(null, './public/img/menu/')
+  },
+  filename: async function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({
+  storage: storage
+})
 
 
 const initRoutes = (app) => {
-    app.get('/', homeController().index)
-    app.get('/login',guest, authController().login)
-    app.post('/login', authController().postLogin)
-    app.get('/register',guest, authController().register)
-    app.post('/register',authController().postRegister)
-    app.post('/logout',authController().logout)
-    
-    app.get('/cart', cartController().index)
-    app.post('/update-cart',cartController().update)
+  app.get('/', homeController().index)
+  app.get('/login', guest, authController().login)
+  app.post('/login', authController().postLogin)
+  app.get('/register', guest, authController().register)
+  app.post('/register', authController().postRegister)
+  app.post('/logout', authController().logout)
 
-    // Customer Routes
-    app.post('/orders',auth, orderController().store)
-    app.post('/cancel-order',auth, orderController().cancel)
-    app.post('/remove-from-cart',auth, orderController().removeFromCart)
-    app.post('/promo-code',auth, orderController().applyPromoCode)
-    app.get('/customer/orders',auth, orderController().index)
-    app.get('/customer/orders/:id',auth, orderController().show)
+  app.get('/cart', cartController().index)
+  app.post('/update-cart', cartController().update)
 
-    // Admin Routes
-    app.get('/admin/orders', admin, adminOrderController().index)
-    app.get('/promocodes', admin, adminOrderController().promoCode)
-    app.get('/active-codes', admin, adminOrderController().getActiveCodes)
-    app.get('/dishes', admin, adminFeatureController().index)
-    app.get('/dishes/add', admin, adminFeatureController().addPage)
-    app.get('/dishes/:id', admin, adminFeatureController().showDish)
-    //app.post('/upload', admin, adminFeatureController().upload)
-    app.post('/dishes/save', admin, adminFeatureController().saveChanges)
-    app.post('/dishes/remove', admin, adminFeatureController().removeDish)
-    app.post('/dishes/add-dish', admin, upload.single('dishImage'),adminFeatureController().store)
-    app.post('/add-promo-code',admin,adminOrderController().addCode)
-    app.post('/disable-promo-code',admin,adminOrderController().disableCode)
-    app.post('/admin/order/status', admin, statusController().update)
+  // Customer Routes
+  app.post('/orders', auth, orderController().store)
+  app.post('/cancel-order', auth, orderController().cancel)
+  app.post('/remove-from-cart', auth, orderController().removeFromCart)
+  app.post('/promo-code', auth, orderController().applyPromoCode)
+  app.get('/customer/orders', auth, orderController().index)
+  app.get('/customer/orders/:id', auth, orderController().show)
+
+  // Admin Routes
+  app.get('/admin/orders', admin, adminOrderController().index)
+  app.get('/promocodes', admin, adminOrderController().promoCode)
+  app.get('/active-codes', admin, adminOrderController().getActiveCodes)
+  app.get('/dishes', admin, adminFeatureController().index)
+  app.get('/dishes/add', admin, adminFeatureController().addPage)
+  app.get('/dishes/:id', admin, adminFeatureController().showDish)
+  app.post('/dishes/save', admin, adminFeatureController().saveChanges)
+  app.post('/dishes/remove', admin, adminFeatureController().removeDish)
+  app.post('/dishes/add-dish', admin, upload.single('dishImage'), adminFeatureController().store)
+  app.post('/add-promo-code', admin, adminOrderController().addCode)
+  app.post('/disable-promo-code', admin, adminOrderController().disableCode)
+  app.post('/admin/order/status', admin, statusController().update)
 }
 
 module.exports = initRoutes
