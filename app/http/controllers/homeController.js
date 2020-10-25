fc31cda193a409ada2fa496b83c775cdf406ed1f
Siddharth-IT59 <65977324+Siddharth-IT59@users.noreply.github.com>
2020-10-26 00:09:39 +0530

    admin cannot order

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -5,7 +5,7 @@ const homeController = () =>{
         async index(req, res){
             const dishes = await Dish.find({})
             req.header('Cache-Control','no-cache,private,no-store,must-revalidate,max-stale=0,post-check=0,pre-check=0')
-            return res.render('home', { dishes: dishes })
+            return res.render('home', { dishes: dishes , user: req.user })
         }
     }
 }
